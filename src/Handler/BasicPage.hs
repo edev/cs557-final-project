f3f58cedde6ba22a15a11fa320f049af544d9c12
Dylan Laufenberg <dylan.laufenberg@gmail.com>
2019-03-19 12:23:53 -0700

    Add comments to getBasicPageR.

--- src/Handler/BasicPage.hs
+++ src/Handler/BasicPage.hs
@@ -8,8 +8,14 @@ import Import
 
 getBasicPageR :: PagePath -> Handler Html
 getBasicPageR (PagePath pagePath) = do
+      -- Try to retrieve the page from the database by looking up
+      -- the path we received from the BasicPageR route.
       maybePage <- runDB $ getBy (UniquePath (Just pagePath))
       case maybePage of
+        -- We received a valid page! Great!
+        -- We'll unpack just what we need, discarding the rest,
+        -- and render the layout by following the same basic steps
+        -- as the default HomePageR handler.
         Just (Entity _ (BasicPage title _ _ _ content)) 
           -> defaultLayout $ do
                 setTitle (toHtml title)
